0b4fbd34ee822199a6645fa2e6d5bffe1551536c
Em <b4bareis@gmail.com>
2017-09-23 21:03:45 -0500

    added ability for bye to get the

--- foilGive.py
+++ foilGive.py
@@ -1,20 +1,33 @@
 import sys
 import random
 
-def main():
-    a = int(sys.argv[1])
-    c = int(sys.argv[2])
+def makeTables(players, foils):
     b = 0
+    indexer = 0
+    if(players % 2 <> 0):
+        indexer = 1
     tables = []
-    if(a < c*2):
-        while (b < a):
-            d = "table: " + str(random.randint(1,c)) + " seat: " + str(random.randint(1,2))
-            if(d in tables):
-               continue
-            tables.append(d)
-            print d
-            b = b + 1
+    while (b < foils):
+        table = str(random.randint(1,(players/2)) - indexer)
+        if(table == "0"):
+            d = "Bye gets promo"
+        else:
+            d = "table: " + table + " seat: " + str(random.randint(1,2))
+        if(d in tables):
+           continue
+        tables.append(d)
+        print d
+        b = b + 1
+
+def main():
+    #idea for bye: take total number of players.  if odd, halve, round up, zero index. zero table gets promo.
+    foils = int(sys.argv[1])
+    players = int(sys.argv[2])    
+    
+    if(foils < players):
+        makeTables(players, foils)        
     else:
         print "ERROR: number of randoms cannot be greater than players"
 if __name__ == "__main__":
     main()
+
